b4d1efde9aebdae30ff0285b391d1f094f474989
Zoha Khan <zohakhan11004@gmail.com>
2024-10-31 17:05:01 +0500

    avator add

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,39 +1,47 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
 import { ContactUser, Email } from "../components/svgs";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 
 const ContactUs = () => {
-  const [fullname , setFullname] = useState('');
-  const [email , setEmail] = useState('');
-  const [message , setMessage] = useState('');
-  const [error , setError] = useState([]);
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState([]);
 
-  const handleSubmit = async (e:any) => {
-   e.preventDefault();
-   console.log(fullname);
-   console.log(email);
-   console.log(message);
-   
-   
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(fullname);
+    console.log(email);
+    console.log(message);
 
-   const res = await fetch("api/contact",{
-    method:"POST" ,
-    headers:{
-      "Content-type":"application/json"
-    },
-    body:JSON.stringify({
-      fullname,
-      email,
-      message,
-    })
-
-   })
-   const {msg} = await res.json();
-   setError(msg)
-   console.log(error);
-   
-  }
+    const res = await fetch("api/contact", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        fullname,
+        email,
+        message,
+      }),
+    });
+    const { msg } = await res.json();
+    setError(msg);
+    console.log(error);
+  };
   return (
     <div>
       <section className="px-4">
@@ -67,8 +75,8 @@ const ContactUs = () => {
                   <ContactUser className="w-4 h-4 text-gray-500 dark:text-gray-400" />
                 </div>
                 <input
-                onChange={(e)=>setFullname(e.target.value)}
-                value={fullname}
+                  onChange={(e) => setFullname(e.target.value)}
+                  value={fullname}
                   type="text"
                   id="name"
                   name="name"
@@ -87,8 +95,8 @@ const ContactUs = () => {
                   <Email className="w-4 h-4 text-gray-500 dark:text-gray-400" />
                 </div>
                 <input
-                 onChange={(e)=>setEmail(e.target.value)}
-                 value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
                   type="text"
                   id="email"
                   name="email"
@@ -104,8 +112,8 @@ const ContactUs = () => {
               </label>
               <div className="relative mb-4">
                 <textarea
-                onChange={(e)=>setMessage(e.target.value)}
-                value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  value={message}
                   rows={8}
                   id="message"
                   name="message"
@@ -113,31 +121,42 @@ const ContactUs = () => {
                   placeholder="Type your message here"
                 />
               </div>
-              <button
-                type="submit"
-                className="text-white bg-[#1788ae] hover:bg-[#1788aeb7] focus:ring-4 focus:ring-[#1788ae] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 w-full focus:outline-none"
-              >
-                Send
-              </button>
-            </form>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <button
+                    type="submit"
+                    className="text-white bg-[#1788ae] hover:bg-[#1788aeb7] focus:ring-4 focus:ring-[#1788ae] font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 w-full focus:outline-none"
+                  >
+                    Send
+                  </button>
+                </AlertDialogTrigger>
 
+                <AlertDialogContent className="flex flex-col justify-end w-[300px] sm:w-[400px] max-h-[200px] border rounded-lg">
+                  {error?.length > 0 &&
+                    error.map((e, index) => (
+                      <div
+                        key={index}
+                        className="text-red-600 py-2 bg-transparent"
+                      >
+                        {e}
+                      </div>
+                    ))}
+
+                  <AlertDialogFooter>
+                    <AlertDialogCancel className="bg-black text-white">
+                      Cancel
+                    </AlertDialogCancel>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </form>
           </div>
         </div>
-
-        <div className="bg-slate-100 flex flex-col">
-  {error && error.map((e, index) => {
-    return (
-      <div key={index} className="text-red-600 px-5 py-2">
-        {e}
-      </div>
-      );
-  })}
-</div>
-
-
       </section>
     </div>
   );
 };
 
 export default ContactUs;
+
+
